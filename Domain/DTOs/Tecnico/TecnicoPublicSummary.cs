143334109c0bdf7bb7dff1f1a1576944a135139c
Matheus Mikio <matheusmikio15@gmail.com>
2026-04-04 18:21:04 -0300

    atributo de disponiblidade do tecnico melhorado para visualização

--- Domain/DTOs/Tecnico/TecnicoPublicSummary.cs
+++ Domain/DTOs/Tecnico/TecnicoPublicSummary.cs
@@ -6,6 +6,15 @@ namespace Domain.DTOs.Tecnico
         public Guid PublicId { get; private set; } = tecnico.PublicId;
         public string Nome { get; private set; } = tecnico.Nome;
         public string Descricao { get; private set; } = tecnico.Descricao;
-        public bool EstaDisponivelHoje { get; private set; } = tecnico.Horarios?.Any(h => h.Dia == DateTime.Now.DayOfWeek) ?? false;
+        public bool EstaDisponivelHoje { get; private set; } = EstaDisponivelNoMomento(tecnico.Horarios);
+
+        private static bool EstaDisponivelNoMomento(IList<models.HorarioDisponibilidade>? horarios)
+        {
+            var agora = DateTime.Now;
+            var diaAtual = agora.DayOfWeek;
+            var horaAtual = TimeOnly.FromDateTime(agora);
+
+            return horarios?.Any(h => h.Dia == diaAtual && horaAtual >= h.Inicio && horaAtual <= h.Fim) ?? false;
+        }
     }
 }
